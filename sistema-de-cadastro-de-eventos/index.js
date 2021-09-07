e86bb1d9db866b6d1126989abbf2f165e1b4115d
xPyXer <MateusLeoSilva666@gmail.com>
2021-09-07 11:56:21 -0300

    Check

---
+++ sistema-de-cadastro-de-eventos/index.js
@@ -0,0 +1,22 @@
+var data = "7/09/2021"
+console.log(data)
+if(data < "7/09/2021") {
+    console.log('Data inválida')
+};
+
+var nome = String(prompt("Digite seu nome: "))
+var idade = Number(prompt("Digite sua idade: "));
+
+if(idade >= 18) {
+    console.log()
+} else {
+    console.log('Acesso negado! Você precisa ter mais que 18 anos.')
+};
+
+var participantes = 71
+
+while(participantes < 100) {
+    cadastar = true;
+} if (participantes > 100) {
+    alert("Cadastro recusado! Limite excedido!")
+}
