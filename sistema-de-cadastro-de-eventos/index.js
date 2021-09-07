b868f1457efd2973e30169914c8a276de4d5804a
xPyXer <MateusLeoSilva666@gmail.com>
2021-09-07 14:55:49 -0300

    Commit

--- sistema-de-cadastro-de-eventos/index.js
+++ sistema-de-cadastro-de-eventos/index.js
@@ -1,20 +1,24 @@
+// Verifica se a data é posterior a "7/09/2021"
 var data = "7/09/2021"
 console.log(data)
 if(data < "7/09/2021") {
     console.log('Data inválida')
 };
 
+// Abre um prompt para inserir sua idade
 var idade = Number(prompt("Digite sua idade: "));
 console.log(idade);
 
+// Verifica a idade inserida
 if(idade >= 18) {
     console.log('Acesso concedido, prossiga com o cadastro!')
 } else {
     console.log('Acesso negado! Você precisa ter mais que 18 anos.')
 };
 
-var participantes = 71
 
+var participantes = 71
+// Se tiver menos de 100 cadastrados é possível cadastrar, caso contrário não será possível
 while(participantes < 100) {
     cadastar === true;
 } if (participantes > 100) {
